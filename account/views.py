06c8a77ab5cfc0fface2562c9a283e10f369841c
MugumoPerm <110578750+MugumoPerm@users.noreply.github.com>
2023-09-02 13:49:32 +0300

    update

--- account/views.py
+++ account/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
-
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 
@@ -9,6 +9,9 @@ def login_user(request ):
 
 
 def register_user(request):
+    form = UserCreationForm()
+    
+    context = {"form": form}
     return render(request, template_name="register.html")
 
 def home(request):
