d9f315a79f3fc1d488310d248477b84776523fad
MugumoPerm <110578750+MugumoPerm@users.noreply.github.com>
2023-09-04 01:12:51 +0300

    update the templates, views,urls and database

--- account/urls.py
+++ account/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path("", views.home, name= "home"),
     path("login/", views.login_user, name= "login"),
     path("register/", views.register_user, name= "register"),
+    path("songs/", views.songs, name="songs"),
+    path("logout/", views.logout_user,name="logout"),
 ]
--- account/views.py
+++ account/views.py
@@ -2,10 +2,28 @@ from django.shortcuts import render, redirect
 from django.http import HttpRequest, HttpResponse
 from django.contrib.auth.forms import UserCreationForm
 
+from django.contrib.auth import authenticate, login, logout
+
+from django.contrib.auth.decorators import login_required
+
+from django.contrib import messages
 # Create your views here.
 from .forms import CreateUserForm
 
 def login_user(request ):
+
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect("songs")
+        else:
+            messages.error(request, "Invalid Username or password")
+
     return render(request, template_name="login.html")
 
 
@@ -14,6 +32,8 @@ def register_user(request):
         form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
+            name = form.cleaned_data.get("username")
+            messages.success(request, "account for "+ name + " was created succesfully")
             return redirect('login')
     else:
         form = CreateUserForm()
@@ -25,6 +45,11 @@ def register_user(request):
 
 def home(request):
     return render(request, template_name="index.html")
-
+@login_required(login_url='login')
 def songs(request):
+
     return render(request, template_name="songs.html")
+
+def logout_user(request):
+    logout(request)
+    return redirect("login")
