040eacc8afcfeb03f9247cdc0d954ca9b319f812
MugumoPerm <110578750+MugumoPerm@users.noreply.github.com>
2023-09-12 23:15:54 +0300

    update

--- account/views.py
+++ account/views.py
@@ -15,7 +15,7 @@ from .forms import CreateUserForm
 def login_user(request):
 
     if request.method == "POST":
-        username = request.POST.get('username')
+        username = (request.POST.get('username')).lower()
         password = request.POST.get('password')
 
         user = authenticate(request, username=username, password=password)
