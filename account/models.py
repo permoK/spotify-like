29400ca0c0ad50830077a9e4877042800db8ea0a
MugumoPerm <perminusk21@gmail.com>
2023-09-16 21:36:26 +0300

    update

--- account/models.py
+++ account/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Artist(models.Model):
+    firstname = models.CharField(max_length=200)
+    lastname = models.CharField(max_length=200)
+    email = models.EmailField(max_length=200)
+    Age = models.IntegerField()
+    password = models.CharField(max_length=10)
+    
+
+    def __str__(self):
+        return self.firstname + " " + self.lastname
